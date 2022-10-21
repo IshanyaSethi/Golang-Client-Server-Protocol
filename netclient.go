293734636b85073873645560f39487f39c61deac
IshanyaSethi <76902436+IshanyaSethi@users.noreply.github.com>
2022-10-21 12:36:39 +0530

    Add files via upload

---
+++ netclient.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	arguments := os.Args
+	if len(arguments) == 1 {
+		fmt.Println("Please provide host:port.")
+		return
+	}
+
+	CONNECT := arguments[1]
+	con, err := net.Dial("tcp", CONNECT)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer con.Close()
+
+	clientReader := bufio.NewReader(os.Stdin)
+	serverReader := bufio.NewReader(con)
+
+	for {
+		// Waiting for the client request
+		clientRequest, err := clientReader.ReadString('\n')
+
+		switch err {
+		case nil:
+			clientRequest := strings.TrimSpace(clientRequest)
+			if _, err = con.Write([]byte(clientRequest + "\n")); err != nil {
+				log.Printf("failed to send the client request: %v\n", err)
+			}
+		case io.EOF:
+			log.Println("client closed the connection")
+			return
+		default:
+			log.Printf("client error: %v\n", err)
+			return
+		}
+
+		// Waiting for the server response
+		serverResponse, err := serverReader.ReadString('\n')
+
+		switch err {
+		case nil:
+			log.Println(strings.TrimSpace(serverResponse))
+		case io.EOF:
+			log.Println("server closed the connection")
+			return
+		default:
+			log.Printf("server error: %v\n", err)
+			return
+		}
+	}
+}
