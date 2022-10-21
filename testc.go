cdfa150d9a2fc4b95060148a4542b4077c9d47df
IshanyaSethi <76902436+IshanyaSethi@users.noreply.github.com>
2022-10-21 12:34:51 +0530

    Update testc.go

--- testc.go
+++ testc.go
@@ -3,36 +3,57 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
-	"strings"
 )
 
+// main serves as the program entry point
 func main() {
-	arguments := os.Args
-	if len(arguments) == 1 {
-		fmt.Println("Please provide host:port.")
-		return
-	}
+	// obtain the port and prefix via program arguments
 
-	CONNECT := arguments[1]
-	c, err := net.Dial("tcp", CONNECT)
+	port := fmt.Sprintf(":%s", os.Args[1])
+	prefix := os.Args[2]
+
+	// create a tcp listener on the given port
+	listener, err := net.Listen("tcp", port)
 	if err != nil {
-		fmt.Println(err)
-		return
+		fmt.Println("failed to create listener, err:", err)
+		os.Exit(1)
 	}
+	fmt.Printf("listening on %s, prefix: %s\n", listener.Addr(), prefix)
 
+	// listen for new connections
 	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print(":: ")
-		text, _ := reader.ReadString('\n')
-		fmt.Fprintf(c, text+"\n")
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("failed to accept connection, err:", err)
+			continue
+		}
 
-		message, _ := bufio.NewReader(c).ReadString('\n')
-		fmt.Print("->: " + message)
-		if strings.TrimSpace(string(text)) == "STOP" {
-			fmt.Println("TCP client exiting...")
+		// pass an accepted connection to a handler goroutine
+		go handleConnection(conn, prefix)
+	}
+}
+
+// handleConnection handles the lifetime of a connection
+func handleConnection(conn net.Conn, prefix string) {
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	for {
+		// read client request data
+		bytes, err := reader.ReadBytes(byte('\n'))
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("failed to read data, err:", err)
+			}
 			return
 		}
+		fmt.Printf("request: %s", bytes)
+
+		// prepend prefix and send as response
+		line := fmt.Sprintf("%s %s", prefix, bytes)
+		fmt.Printf("response: %s", line)
+		conn.Write([]byte(line))
 	}
 }
