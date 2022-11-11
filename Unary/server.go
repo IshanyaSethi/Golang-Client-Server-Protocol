e8c9794b111d7682e4a202fae2e4035e64fc765f
IshanyaSethi <76902436+IshanyaSethi@users.noreply.github.com>
2022-11-11 16:24:24 +0530

    Update server.go

--- Unary/server.go
+++ Unary/server.go
@@ -12,7 +12,7 @@ import (
 type server struct{}
 
 func main() {
-	lis, err := net.Listen("tcp", "0.0.0.0:50051")
+	lis, err := net.Listen("tcp", "0.0.0.0:1235")
 	if err != nil {
 		log.Fatalf("Failed to listen %v", err)
 	}
